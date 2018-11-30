d92301f792dd899aeed744af1dc8c13fdea3553a
litao <litao@winshang.com>
2018-11-30 17:04:44 +0800

    del

--- src/main/java/com/example/demo/dao/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.example.demo.entity.Post;
-
-public interface PostRepository extends ElasticsearchRepository<Post, String>{
-
-}
--- src/main/java/com/example/demo/dao/YaGaoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
-
-import com.example.demo.entity.YaGao;
-
-public interface YaGaoRepository extends ElasticsearchRepository<YaGao,String>{
-	
-}
